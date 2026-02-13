07fa126606c319615e3b7fc79cb8f0eb887f5950
bhumit2025 <2025.bhumit.peshavariya@ves.ac.in>
2026-02-13 18:52:45 +0530

    Create AddNumber.java

---
+++ AddNumber.java
@@ -0,0 +1,12 @@
+package com.example.springbootapp;
+
+public class AddNumbers {
+
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
